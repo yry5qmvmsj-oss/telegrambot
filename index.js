0eb7edb4f745568cb26d92b07821c06791b541fc
yry5qmvmsj-oss <yry5qmvmsj@privaterelay.appleid.com>
2026-04-24 02:25:38 +0500

    Add Express server and update bot response message

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const TelegramBot = require('node-telegram-bot-api');
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server ishlayapti 🚀");
+});
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, "Bot ishlayapti 🚀");
+bot.on("message", (msg) => {
+  bot.sendMessage(msg.chat.id, "Bot ishlayapti 🤖");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on", PORT);
 });
