a33cde952822e3a6de593f5e06ff5ca84f0ad013
yry5qmvmsj-oss <yry5qmvmsj@privaterelay.appleid.com>
2026-04-23 21:01:40 +0500

    index.js erstellen

---
+++ index.js
@@ -0,0 +1,8 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, "Bot ishlayapti 🚀");
+});
